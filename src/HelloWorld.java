3e137a56c66d8deb4fbd60ff41bf75cda7d5574c
ocp1973 <ocpascual@yahoo.com>
2021-08-27 14:53:42 +0800

    init

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * This class contains a method to print Hello World. My first Java program.
+ * 
+ * @author Orlan Pascual
+ * @version 1.0
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
